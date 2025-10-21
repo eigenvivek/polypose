2f5468f306e30ed021f4c0f199c04e84d7e8ccd0
eigenvivek <vivekg@mit.edu>
2025-10-21 11:17:05 -0400

    Refactor weights

--- src/polypose/weights/weights.py
+++ src/polypose/weights/weights.py
@@ -1,8 +1,8 @@
-import cupy as cp
 import torch
-from cupyx.scipy.ndimage import distance_transform_edt as _distance_transform_edt
 from torchio import Subject
 from tqdm import tqdm
+
+from .sdf import distance_transform_edt, signed_distance_field
 
 
 def inverse_distance(D: torch.Tensor, eps: float) -> torch.Tensor:
@@ -77,29 +77,3 @@ def compute_weights(
         weights = weights / weights.sum(dim=0, keepdim=True)
 
     return segmentations, weights.to(torch.float32)
-
-
-def distance_transform_edt(
-    x: torch.Tensor,  # Binary 3D segmentation mask
-    spacing: float | tuple[float] = None,  # Voxel spacing
-):
-    """
-    If outside x, min Euclidean distance to surface of x
-    If inside x, 0
-    """
-    x = cp.asarray(x)
-    x = _distance_transform_edt(x, sampling=spacing)
-    return torch.as_tensor(x)
-
-
-def signed_distance_field(
-    x: torch.Tensor,  # Binary 3D segmentation mask
-    spacing: float | tuple[float] = None,  # Voxel spacing
-):
-    """
-    If outside x, min Euclidean distance to surface of x
-    if inside x, negative min Euclidean distance to surface of x
-    """
-    inside = distance_transform_edt(x, spacing)
-    outside = distance_transform_edt(~x, spacing)
-    return inside - outside
